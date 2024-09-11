2d9301602d589dade69a29183fc9b63ba66c1234
Moustafa Bendib <46543561+MoustafaSoftbend@users.noreply.github.com>
2024-09-11 08:58:57 +0000

    fixed some deckgl.d.ts file

---
+++ deckgl.d.ts
@@ -0,0 +1,13 @@
+declare module "@deck.gl/layers" {
+  export class GeoJsonLayer {
+    constructor(props: any);
+  }
+
+  export class ScatterplotLayer {
+    constructor(props: any);
+  }
+
+  export class LineLayer {
+    constructor(props: any);
+  }
+}
--- deckgl.d.tsx
+++
@@ -1,4 +0,0 @@
-declare module "@deck.gl/react" {
-  import * as React from "react";
-  export default class DeckGL extends React.Component<any> {}
-}
